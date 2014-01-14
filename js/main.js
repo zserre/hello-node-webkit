4b2f45236553547e3898e17566b13e7b2cc43a65
Zach Serre <zachserre@10333.local>
2014-01-14 17:37:47 -0600

    Added Git functionality

--- js/main.js
+++ js/main.js
@@ -1,26 +1,58 @@
-global.$ = $;
+var shell = require('nw.gui').Shell
+var spawn = require('child_process').spawn
+var exec = require('child_process').exec, child
+var fs = require('fs')
+var path = require('path')
 
-var abar = require('address_bar');
-var folder_view = require('folder_view');
-var path = require('path');
-var shell = require('nw.gui').Shell;
+function chooseFile(name) {
+  var chooser = document.querySelector(name)
+  chooser.addEventListener("change", function(evt) {
+    console.log(this.value)
+  }, false)
+
+  chooser.click()
+}
+
+function runShellCommand(cmd, cb) {
+  child = exec(cmd, {cwd: '/Users/zachserre/workspace/ngin'}, function (error, stdout, stderr) {
+      cb(error, stdout, stderr) 
+  })
+
+}
 
 $(document).ready(function() {
-  var folder = new folder_view.Folder($('#files'));
-  var addressbar = new abar.AddressBar($('#addressbar'));
 
-  folder.open(process.cwd());
-  addressbar.set(process.cwd());
+  $('#frmGitRepo').on('submit', function(e) {
+    e.preventDefault()
+  })  
 
-  folder.on('navigate', function(dir, mime) {
-    if (mime.type == 'folder') {
-      addressbar.enter(mime);
-    } else {
-      shell.openItem(mime.path);
-    }
-  });
+  $('#btnBrowse').click(function() {
+    chooseFile('#fileDialog')
+  })
 
-  addressbar.on('navigate', function(dir) {
-    folder.open(dir);
-  });
-});
+  $('#btnRefreshProjects').click(function() {
+
+    $('#tblCmdContent').addClass('fouc')
+  
+    runShellCommand('git branch', function(err, stdout, stderr) {
+      if (stdout !== "") {
+        var tLines = stdout.split('\n') || []
+        var cmdHtml = ""
+        var curBranch = ""
+        if (tLines.length > 0) {
+
+          for(var i = 0; i < tLines.length - 1; i++ ) {
+            if(tLines[i].substring(0,1) == "*") {
+              cmdHtml = cmdHtml + '<tr><td><span class="greenTxt">' + tLines[i] + '</span></td></tr>'
+            } else {
+              cmdHtml = cmdHtml + '<tr><td>' + tLines[i] + '</td></tr>'
+            }
+            
+          }
+          $('#tblCmdContent').html('<thead><th>Branches</th></thead><tbody>'  + cmdHtml + '</tbody')
+          $('#tblCmdContent').removeClass('fouc')
+        }
+      }
+    })
+  })
+})
